b0d48fc5e59252fc5b65a0e2354580c37759bed5
Burak <buraktimur000@gmail.com>
2021-02-19 17:42:51 +0300

    shoppingcart123.py

---
+++ shoppingcart123.py
@@ -0,0 +1,19 @@
+list = []
+
+while True:
+    command = input("Please add entries to the list,then type rearrange to remove duplicates: ").lower()
+    if command != "rearrange":
+        list.append(command)
+
+    elif command == "rearrange":
+        if list.count == 0:
+            print("Please add entries to the list before rearranging.")
+
+        else:
+            break
+
+for item in list:
+    while list.count(item) > 1:
+        list.remove(item)
+
+print(list)
